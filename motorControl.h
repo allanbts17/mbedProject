e60de5d38898a36976542c3473550513e3fcfba6
allanbts17 <82842569+allanbts17@users.noreply.github.com>
2021-08-04 15:59:19 -0500

    Optimized encoder read

--- motorControl.h
+++ motorControl.h
@@ -1,6 +1,7 @@
 #include "mbed.h"
 #include "motorDriver.h"
 #include "PID/pid.h"
+#include "list.h"
 
 class motorControl{
 	private:
@@ -9,6 +10,7 @@ class motorControl{
 		motorDriver *m1; 
 		DigitalIn *ia;
 		DigitalIn *ib;
+		List *lst;
 		
 		//Interrupts
 		InterruptIn *a;
@@ -21,6 +23,7 @@ class motorControl{
 		double output_limit;
 		int ind;
 		int stage;
+		bool clockwise;
 		bool start_pulse;
 		
 		//PID values
@@ -35,7 +38,6 @@ class motorControl{
 		//Debug values
 		int debug_pulse;
 		
-
 	public:
 		//Constructor
 		motorControl();
@@ -72,13 +74,20 @@ class motorControl{
 	/************************************** Action functions****************************/
 	//Transforming the PID signal for the motor driver
 	void signal(double pid){
-	int control_limit = 1000;
-	//An interpolation function to fix value to a 1000 to -1000 range, and then divide by 1000
-	float control = -control_limit + (control_limit/output_limit)*(pid+output_limit);
-	control /= 1000; //////// revisar, podría ser necesario que los números sean float para dividirse así
+		int control_limit = 1000;
+		//An interpolation function to fix value to a 1000 to -1000 range, and then divide by 1000
+		float control = -control_limit + (control_limit/output_limit)*(pid+output_limit);
+		//control /= 1000; //////// revisar, podrÃ­a ser necesario que los nÃºmeros sean float para dividirse asÃ­
+		control = control/1000.0f;
 		//if (abs(control) < 0.1f) {m1->brake();pc->printf("Reached: %d\n",pulse);}
-		if(control > 0) m1->forward(control);
-		else if(control < 0) m1->reverse(-control);
+		if(control > 0){
+			m1->forward(control);
+			clockwise = true;
+		}
+		else if(control < 0) {
+			m1->reverse(-control);
+			clockwise = false;
+		}
 	}
 
 /************************** Run function *****************************/
@@ -96,99 +105,49 @@ class motorControl{
 		double inc = pid->calculate(pulse_setpoint*1.0, pulse*1.0, dt);
 		//pc->printf("Setpoint: %5.2f, Pulse: %5.2f, Output: %5.2f,delta: %5.2f\n",pulse_setpoint*1.0,pulse*1.0,inc,dt);
 		//pc->puts("Setpoint:");
-	
-		//uint16_t sp = (uint16_t)pulse_setpoint;
-		//valueSerial(sp);
-		valueSerial((uint16_t)pulse_setpoint);
-		//pc->puts(", Pulse:");
-		valueSerial((uint16_t)pulse);
-		//pc->puts("\n");
+		bool sendString = false;
+		
+		if(!sendString){
+			valueSerial((uint16_t)pulse_setpoint);
+			valueSerial((uint16_t)pulse);
+			
+			//valueSerial((uint16_t)inc);
+		}
+		else{
+			pc->printf("%5.2f, %5.2f, %5.2f\n",pulse_setpoint*1.0,pulse*1.0,inc);
+		}
+
 		signal(inc);
 	}
 
 /****************************** Encoder Read function **************/
 	//When encoder moves, this function works
 	void encoderRead(){
-		//pc.putc('a');
 		
-		switch(encoder->read()){
-		case 0: //1
-			//If this is second pulse it has to move twice.
-			if(start_pulse) {
-				start_pulse = false;
+		int add = clockwise? 1:-1;
+		pulse += add;
+		/*int encoderPos = encoder->read();
+		if(!start_pulse){
+				lst->setIndexByState(encoderPos);
+				start_pulse = true;
+			}else{
+				int add = clockwise? 1:-1;
+				pulse += add;
 				
-				//Add or rest two times
-				if(stage == 4) pulse++; //clockwise
-				else if(stage == 2) pulse--; //counter-clockwise
-			}
-			if(stage == -1) start_pulse = true; //If it's first pulse, is imposible to khown direction,
-			//so in next pulse it have to count twice.
-			//To sum or rest a pusle, depending on last stage
-			else{
-					if(stage == 4) pulse++; //clockwise
-					else if(stage == 2) pulse--; //counter-clockwise
-			}
-			stage = 1; //Updating stage
-			break;
-			
-		case 1: //2
-			if(start_pulse) {
-				start_pulse = false;
-				
-				//Add or rest two times
-				if(stage == 1) pulse++; //clockwise
-				else if(stage == 3) pulse--; //counter-clockwise
-			}
-			if(stage == -1) start_pulse = true;
-			else{
-					if(stage == 1) pulse++; //clockwise
-					else if(stage == 3) pulse--; //counter-clockwise
-			}
-			stage = 2;
-			break;
-			
-		case 3: //3
-			if(start_pulse) {
-				start_pulse = false;
-				
-				//Add or rest two times
-				if(stage == 1) pulse++; //clockwise
-				else if(stage == 4) pulse--; //counter-clockwise
-			}
-			if(stage == -1) start_pulse = true;
-			else{
-					if(stage == 1) pulse++; //clockwise
-					else if(stage == 4) pulse--; //counter-clockwise
-			}
-			stage = 3;
-			break;
-			
-		case 2://4
-			if(start_pulse) {
-				start_pulse = false;
-				
-				//Add or rest two times
-				if(stage == 3) pulse++; //clockwise
-				else if(stage == 1) pulse--; //counter-clockwise
-			}
-			if(stage == -1) start_pulse = true;
-			else{
-					if(stage == 3) pulse++; //clockwise
-					else if(stage == 1) pulse--; //counter-clockwise
-			}
-			stage = 4;
-			break;
-		}
-		
-		//pc->printf("1");
-		//pc -> printf("%d",encoder -> read());
+				lst->getCurrentState();
+				stage = clockwise? lst->next():lst->previous();
+			}*/
 		run();
 	}
+	
+	
 
 /***************************** Init function ***********************/
 	void init(){
 		t.start();
 		last_time = t.read_us();
+		
+		lst->initValues();
 		
 //	PID pid = PID(dt, max, min, kp, kd, ki);
 //	ia->mode(PullDown);
@@ -221,6 +180,7 @@ m1(new motorDriver(p22,p23,p21)),
 
 ia(new DigitalIn(p26)),
 ib(new DigitalIn(p27)),
+lst(new List()),
 
 a(new InterruptIn(p26)),
 b(new InterruptIn(p27)),
@@ -235,40 +195,13 @@ kp(1),kd(0.2),ki(0.5),
 
 pid(new PID(dt, max, min, kp, kd, ki)),
 last_time(0),
-stage(-1),start_pulse(false),
-
+stage(-1),
+clockwise(true),
+start_pulse(false),
 debug_pulse(0)
 {}
 
 void motorControl::print(){
 	pc -> printf("%d",encoder -> read());
 }
-/*
-//Pulse values
-int motorControl::pulse = 0;
-int motorControl::last_pulse = 0;
-const int motorControl::input_limit = 7920;
-double motorControl::output_limit = (input_limit/2)*0.20;
-int motorControl::ind = 0;
-
-//PID values
-double motorControl::dt = 0.1;
-double motorControl::max = output_limit;
-double motorControl::min = -output_limit;
-double motorControl::kp = 1;
-double motorControl::ki = 0.5;
-double motorControl::kd = 0.2;
-PID motorControl::pid = PID(dt, max, min, kp, kd, ki);
-
-int motorControl::stage=-1;
-bool motorControl::start_pulse = false;
-*/
-/*BusIn *encoder; = new BusIn(p26,p27); //A y B
-		Serial *pc; = new Serial(p13,p14); //tx y rx
-		motorDriver *m1; = new motorDriver(p22,p23,p21);
-		DigitalIn *ia; = new DigitalIn(p26);
-		DigitalIn *ib; = new DigitalIn(p27);
-		//Interrupts
-		InterruptIn *a; = new InterruptIn(p26);
-		InterruptIn *b; = new InterruptIn(p27);*/
 		
