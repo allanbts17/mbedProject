2796bc4aa8c3943bd180d2185bce9ab09bd9545d
allanbts17 <82842569+allanbts17@users.noreply.github.com>
2021-09-17 11:10:15 -0500

    Update Comm.h

--- Comm.h
+++ Comm.h
@@ -13,6 +13,7 @@ class Comm{
 		float x;
 		float y;
 		Serial *pc;
+		bool packageSend;
 		
 		
 		
@@ -34,9 +35,21 @@ class Comm{
 		
 		void init(){
 			buf = "GET /foo.bar?query=moo HTTP/1.5\r\n";
-			pattern = "&(\d)\r\n(\d)\r\n([\d-][\d.][\d.][\d.][\d])\r\n([\d-][\d.][\d.][\d.][\d])\r\n@";
+			pattern = "&(	\d)\r\n(\d)\r\n([\d-][\d.][\d.][\d.][\d])\r\n([\d-][\d.][\d.][\d.][\d])\r\n@";
 			compile();
 			pc->baud(9600);
+		}
+		
+		bool getPackageSend(){
+			return packageSend;
+		}
+		
+		void setPackageSend(){
+			packageSend = !packageSend;
+		}
+		
+		void setPackageSend(bool set){
+			packageSend = set;
 		}
 		
 		void setBuffer(char *buffer){
@@ -44,22 +57,72 @@ class Comm{
 		}
 	
 		void dataReached(){
+			pc->printf("reached");
 			if(slre_match(&slre, buf, strlen(buf), captures)){
+				pc->printf("matched");
 				matched();
 			}
 		}
 		
 		void dataReached(char *buff){
-			if(slre_match(&slre, buff, strlen(buf), captures)){
+			pc->printf("reached\n");
+			if(slre_match(&slre, buff, strlen(buff), captures)){
+				pc->printf("matched\n");
 				matched();
 			}
+		}
+		
+		void messageVerification(char *buff,int limit){
+			char ax[5];
+			char ay[5];
+			char atotal[1];
+			char aactual[1];
+			
+			int size = limit;//strlen(buff);
+			for(int idx=0;idx<size;idx++){
+				if(buff[idx]=='&' && idx+21 <= limit){
+					if(buff[idx+21]=='@'){						
+						atotal[0] = buff[idx+1];
+						aactual[0] = buff[idx+4];
+						
+						for(int idy=0;idy<5;idy++){
+							ax[idy] = buff[idx+idy+7];
+							ay[idy] = buff[idx+idy+14];							
+						}
+						
+						totalPoint = atoi(atotal);
+						pointNum = atoi(aactual);
+						x = atof(ax);
+						y = atof(ay);
+						
+						//
+						packageSend = true;
+						pc->printf("%d,%d,%5.3f,%5.3f\n",totalPoint,pointNum,x,y);
+						//pc->printf("T: %d, A: %d, X: %5.3f, Y: %5.3f\n",totalPoint,pointNum,x,y);
+						//pc->printf("%d,%d,%5.2f,%5.2f,%d\n",totalPoint,pointNum,x,y,limit);
+						
+						//pc->printf("%c,%d,%d\n",buff[idx],idx,limit);
+					}
+					//pc->printf("yes\n");
+				}
+				/*else{
+				pc->putc('a');
+				pc->putc('\n');
+				}*/
+			}
+		}
+		
+		void printTest(){
+			pc->baud(9600);
+			pc->printf("print test comm 1\n");
 		}
 };
 
 Comm::Comm():
 buf(),pattern(),slre(),captures(),totalPoint(),
 pointNum(),x(),y(),
-pc(new Serial(p13,p14))
+pc(new Serial(p13,p14)),
+packageSend(false)
 {}
 /*
 char *buf;
