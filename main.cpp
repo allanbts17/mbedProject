1261c0124371d9f448ebdaa015bd76d0e15e7219
allanbts17 <82842569+allanbts17@users.noreply.github.com>
2021-11-04 21:44:19 -0500

    Insert angles for motor

--- main.cpp
+++ main.cpp
@@ -2,13 +2,15 @@
 #include "motorControl.h"
 #include "slre.h"
 #include "Comm.h"
+//#include "objectGroup.h"
 
 DigitalOut led1(LED1);
 DigitalOut led2(LED2);
 Serial pc(p13,p14); //tx y rx -- naranja,rojo
-Serial xbee(p9,p10);
+Serial xbee(p9,p10);//tx y rx -- naranja,amarillo
 motorControl controller;
 Comm comm;
+Timer t;
 
 InterruptIn a(p26);
 InterruptIn b(p27);
@@ -19,23 +21,59 @@ char *buf = new char[BUF_LIMIT];
 
 int i = 0;
 int msg;
+bool once = false;
+bool timerFinished = false;
 
-char step[20];
+char step[]="000000"; //0.0000 - 360.00
 
 void moveMsg(){
+
 	while(pc.readable()){
+	if(!once){
+		t.start();
+		once = true;
+		timerFinished = false;
+	}
 		step[i] = pc.getc();
 		i++;
+		pc.printf("%d\n",t.read_ms());
+		
 	}
-	if(i>1)
-		if(!pc.readable() and step[i-1] == 'a') {
-			msg = atoi(step);
+	/*if(!pc.readable() and i>1){
+		
+			pc.printf("%s\n",step);
+		}
+		once = true;
+	}*/
+	/*while(pc.readable()){
+		step[i] = pc.getc();
+		i++;
+	}*/
+	//if(i>1)
+	//	if(!pc.readable() and step[i-1] == 'a') {
+			/*msg = atoi(step);
 			i=0;
 			controller.pulse_setpoint = msg;
 			controller.last_time = controller.t.read_us();
 			controller.setMsg();
-			controller.run();
-		}
+			controller.run();*/
+	//	}
+	//while(pc.readable()){
+	
+		//pc.putc(pc.getc());
+
+		//step[i] = pc.getc();
+		//i++;
+		
+		
+		/*controller.pulse_setpoint = angle;
+		controller.last_time = controller.t.read_us();
+		controller.setMsg();
+		controller.run();*/
+		
+//	}
+	
+	//pc.printf("something");
 }
 
 void xbeeComm(){
@@ -43,8 +81,12 @@ void xbeeComm(){
 		//pc.putc(xbee.getc());
 		
 		if(bufIndex < BUF_LIMIT){
+			if(comm.getPackageSend()){
+				comm.setPackageSend();
+				bufIndex = 0;
+			}
 			buf[bufIndex] = xbee.getc();
-			comm.dataReached(buf);
+			comm.messageVerification(buf,bufIndex);
 			bufIndex++;
 		} else {
 			bufIndex = 0;
@@ -59,44 +101,27 @@ int main()
 	xbee.baud(9600);
 	
 	xbee.attach(xbeeComm);
-//	char *txt = "2.4560";
+	//char *txt = "2.4560";
 	//float num = atof(txt);
 	//pc.printf("%f", num);
-	/*controller.init();*/
-	
+	controller.init();
+	char g[] = "500.05";
+	float f = atof(g);
   pc.printf("Empezo la prueba: \n");
+	pc.printf("%f\n\n",f);
 	comm.init();
 	
-	char *leng = new char[5];
-	char dato = '2';
-	//pc.printf("tesst");
-	pc.putc(dato);
-	leng[0] = 'A';
-	leng[1] = 'B';
-	leng[2] = '\0';
+	while(!timerFinished){
+		if(t.read_ms()>10){
+				pc.printf("%s\n",step);
+				t.stop();
+				t.reset();
+				timerFinished = true;
+				once = false;
+				for(int i=0;i<6;i++)
+					step[i] = '0';
+		}
+	}
 	
-	pc.putc(leng[0]);
-	pc.putc(leng[1]);
 	
-	pc.printf("\n%d",strlen(buf));
-	
-	//pulseTest();
-	//Quitar?
-	/*while(1){
-	}*/
-	/*
-	char *buf = "GET /foo.bar?query=moo HTTP/1.5\r\n";
-	struct slre	slre;
-	struct cap	captures[4 + 1];
-	char *pattern = "^(GET|POST) (\\S+) HTTP/(\\S+?)\r\n";
-	if (!slre_compile(&slre, pattern)) {
-		pc.printf("Error compiling RE: %s\n", slre.err_str);
-	} else if (!slre_match(&slre, buf, strlen(buf), captures)) {
-		printf("Not a valid HTTP request\n" );
-	} else {
-		pc.printf("Request line length: %d\n", captures[0].len);
-		pc.printf("Method: %.*s\n", captures[1].len, captures[1].ptr);
-		pc.printf("URI: %.*s\n", captures[2].len, captures[2].ptr);
-	}*/
 }
-
